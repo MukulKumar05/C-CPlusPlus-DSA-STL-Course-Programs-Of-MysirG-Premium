33fe17cd440d45d7ab545b291109e09a8fd06938
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-06-06 15:57:32 +0530

    Update A18_7.c

--- Assignment18(More Star Pattern Problems)/A18_7.c
+++ Assignment18(More Star Pattern Problems)/A18_7.c
@@ -1,1 +1,55 @@
-
+#include<stdio.h>
+int main()
+{
+    int i, j, k;
+    for (i = 1; i <= 5; i++)
+    {
+        k = 0;
+        for(j = 1; j <= 9; j++)
+        {
+            if(j >= 6 - i && j <= 4 + i)
+            {
+                if(i % 2)
+                {
+                    if((j + i) % 2 == 0)
+                    {
+                        if(j <= 5)
+                        {
+                            k++;
+                            printf("%d", k);
+                        }
+                        else
+                        {
+                            k--;
+                            printf("%d", k);
+                        }
+                    }
+                    else
+                        printf(" ");
+                }
+                else
+                {
+                    if((j + i) % 2 == 0)
+                    {
+                        if(j <= 5)
+                        {
+                            k++;
+                            printf("%d", k);
+                        }
+                        else
+                        {
+                            printf("%d", k);
+                            k--;
+                        }
+                    }
+                    else
+                        printf(" ");
+                }
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
