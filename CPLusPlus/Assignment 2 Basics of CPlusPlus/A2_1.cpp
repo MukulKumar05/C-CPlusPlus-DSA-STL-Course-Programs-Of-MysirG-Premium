b6385cda37deea99f9f099562f9fb56335060776
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-07-07 14:41:58 +0530

    Create A2_1.cpp

---
+++ CPLusPlus/Assignment 2 Basics of CPlusPlus/A2_1.cpp
@@ -0,0 +1,14 @@
+/*
+A2.1 Write a C++ program to print Hello MySirG on the screen.
+(Completed)
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello MySirG"<<endl;
+    return 0;
+}
