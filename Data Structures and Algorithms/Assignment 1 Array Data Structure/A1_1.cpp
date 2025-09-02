5178e31a0612dc5f028a06829c4c48a48d3c0dde
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-09-02 15:05:58 +0530

    Create A1_1.cpp

---
+++ Data Structures and Algorithms/Assignment 1 Array Data Structure/A1_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+/*
+A1.1 Define a class Array to implement array data structure with member
+variables to store capacity of array, last index of the last filled block
+of the array and a pointer to hold the address of the first block of the
+dynamically created array.
+
+(Completed)
+*/
+class Array
+{
+    private:
+        int capacity, lastIndex;
+        int *ptr;
+};
+
+int main()
+{
+    
+    return 0;
+}
