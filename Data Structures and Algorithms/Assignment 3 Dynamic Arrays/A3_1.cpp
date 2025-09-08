1b46d20fa13c0b1182eb1374bed6b31969e119fe
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-09-08 14:50:49 +0530

    Create A3_1.cpp

---
+++ Data Structures and Algorithms/Assignment 3 Dynamic Arrays/A3_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+/*
+A3.1 Define a class DynArray to implement dynamic array data structure
+with member variables to store capacity of array, last index of the
+last filled block of the array and a pointer to hold the address of the
+first block of the dynamically created array.
+
+(Completed)
+*/
+class DynArray
+{
+    private:
+        int capacity;
+        int lastIndex;
+        int *ptr;
+};
+
+int main()
+{
+    
+    return 0;
+}
