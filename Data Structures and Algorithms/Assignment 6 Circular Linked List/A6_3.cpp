7a9ca2f24c6b7ff92826317e5dd4e3b42313547c
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-10-08 14:50:41 +0530

    Add files via upload

---
+++ Data Structures and Algorithms/Assignment 6 Circular Linked List/A6_3.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+/*
+A6.3 In question 1, define a method to insert a data into the list at the 
+beginning.
+
+(Completed)
+*/
+struct node
+{
+    int item;
+    node *next;
+};
+class CLL
+{
+    private:
+        node *last;
+    public:
+        CLL();
+        void insertAtBeginning(int);
+        void displayList();
+        void deleteFromBeginning();
+        ~CLL();
+};
+CLL::CLL()
+{
+    last = nullptr;
+}
+void CLL::insertAtBeginning(int data)
+{
+    node *newPtr = new node;
+    newPtr->item = data;
+    if(last) // if list has >=1 elements
+    {
+        newPtr->next = last->next;
+        last->next = newPtr;
+    }
+    else // if list is empty
+    {
+        newPtr->next = newPtr;
+        last = newPtr;
+    }
+}
+void CLL::displayList()
+{
+    if(last) // if list has >=1 elements
+    {
+        cout<<last->next->item<<" ";
+        if(last->next != last) // > 1 elements present in list
+        {
+            node *ptr = last->next->next;
+            while(ptr != last->next)
+            {
+                cout<<ptr->item<<" ";
+                ptr = ptr->next;
+            }
+        }
+    }
+    else // if list is empty
+        cout<<"Linked list is empty"<<endl;
+}
+void CLL::deleteFromBeginning()
+{
+    if(last) // CLL has >= 1 elements
+    {
+        if(last->next == last) // CLL has only 1 element
+        {
+            delete last;
+            last = nullptr;
+        }
+        else // CLL has > 1 elements
+        {
+            node *ptr = last->next;
+            last->next = ptr->next;
+            delete ptr;
+        }
+    }
+    else // CLL is empty
+        cout<<"Deletion is not possible because linked list is already empty"<<endl;
+}
+CLL::~CLL()
+{
+    while(last)
+        deleteFromBeginning();
+}
+
+int main()
+{
+    CLL mylist;
+    mylist.insertAtBeginning(3);
+    mylist.insertAtBeginning(6);
+    mylist.insertAtBeginning(9);
+    mylist.insertAtBeginning(10);
+    mylist.displayList();
+    return 0;
+}
