f6598eba7cdf1b61e421415b5d692ef78aeaac13
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-10-24 17:17:55 +0530

    Add files via upload

---
+++ Data Structures and Algorithms/Assignment 7 Circular Doubly Linked List/A7_9.cpp
@@ -0,0 +1,204 @@
+#include<iostream>
+using namespace std;
+
+/*
+A7.9 In question 1, define a method to delete a specific node.
+
+(Completed)
+*/
+
+struct node
+{
+    node *prev;
+    int item;
+    node *next;
+};
+
+class CDLL
+{
+    private:
+        node *start;
+    public:
+        CDLL();
+        void insertAtBeginning(int);
+        void displayList();
+        void deleteFromBeginning();
+        ~CDLL();
+        void insertAtEnd(int);
+        node* searchNodeWithItem(int);
+        void insertAfterNode(node*, int);
+        void deleteLast();
+        void deleteNode(node*);
+};
+CDLL::CDLL()
+{
+    start = nullptr;
+}
+void CDLL::insertAtBeginning(int data)
+{
+    node *newPtr = new node;
+    newPtr->item = data;
+    if(start) // list has >=1 elements
+    {
+        newPtr->prev = start->prev;
+        newPtr->next = start;
+        start->prev = newPtr;
+        newPtr->prev->next = newPtr;
+        start = newPtr;
+    }
+    else // list is empty
+    {
+        newPtr->prev = newPtr;
+        newPtr->next = newPtr;
+        start = newPtr;
+    }
+}
+void CDLL::displayList()
+{
+    if(start) // list has >= 1 elements.
+    {
+        cout<<start->item<<" ";
+        if(start->next != start) // if list > 1 elements
+        {
+            node *ptr = start->next;
+            while(ptr != start)
+            {
+                cout<<ptr->item<<" ";
+                ptr = ptr->next;
+            }
+        }
+    }
+    else // list is empty
+        cout<<"Linked List is empty."<<endl;
+}
+void CDLL::deleteFromBeginning()
+{
+    if(start) // list has >= 1 elements
+    {
+        if(start->next == start) // CDLL has only 1 element
+        {
+            delete start;
+            start = nullptr;
+        }
+        else // CDLL has > 1 elements
+        {
+            node *ptr = start;
+            ptr->next->prev = ptr->prev;
+            ptr->prev->next = ptr->next;
+            start = ptr->next;
+            delete ptr;
+        }
+    }
+    else // list is empty
+        cout<<"Deletion is not possible because linked list is already empty."<<endl;
+}
+CDLL::~CDLL()
+{
+    while(start)
+        deleteFromBeginning();
+}
+void CDLL::insertAtEnd(int data)
+{
+    node *newPtr = new node;
+    newPtr->item = data;
+    if(start) // CDLL has >= 1 elements
+    {
+        newPtr->prev = start->prev;
+        newPtr->next = start;
+        start->prev->next = newPtr;
+        start->prev = newPtr;
+    }
+    else // if list is empty
+    {
+        newPtr->prev = newPtr;
+        newPtr->next = newPtr;
+        start = newPtr;
+    }
+}
+node* CDLL::searchNodeWithItem(int itemToSearch)
+{
+    if(start) // if CDLL has >= 1 elements
+    {
+        if(start->item == itemToSearch)
+            return start;
+        if(start->next != start) // CDLL has > 1 elements
+        {
+            node *ptr = start->next;
+            while(ptr != start)
+            {
+                if(ptr->item == itemToSearch)
+                    return ptr;
+                ptr = ptr->next;
+            }
+            return nullptr; // node not found
+        }
+        return nullptr; // node not found
+    }
+    else // if list is empty
+        return nullptr; // list is empty
+}
+void CDLL::insertAfterNode(node *temp, int data)
+{
+    if(temp) // temp is a valid node found in the CDLL
+    {
+        node *newPtr = new node;
+        newPtr->item = data;
+        newPtr->prev = temp;
+        newPtr->next = temp->next;
+        temp->next = newPtr;
+        newPtr->next->prev = newPtr;
+    }
+    else // node temp not found in CDLL
+        cout<<"Insertion not possible because the node passed is not found in the linked list."<<endl;
+}
+void CDLL::deleteLast()
+{
+    if(start) // CDLL has >= 1 elements
+    {
+        if(start->next == start) // CDLL has only 1 element
+        {
+            delete start;
+            start = nullptr;
+        }
+        else // CDLL has > 1 elements
+        {
+            node *ptr = start->prev;
+            ptr->prev->next = start;
+            start->prev = ptr->prev;
+            delete ptr;
+        }
+    }
+    else // list is empty
+        cout<<"Deletion is not possible because the linked list is already empty."<<endl;
+}
+void CDLL::deleteNode(node *temp)
+{
+    if(temp) // node temp is a valid node found in CDLL
+    {
+        if(temp == start) // if node temp is at beginning of list
+            deleteFromBeginning();
+        else if(temp == start->prev) // if node temp is at last of list
+            deleteLast();
+        else // if node temp is neither at beginning nor at the end of linked list
+        {
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+            delete temp;
+        }
+    }
+    else // node temp not found in CDLL or the linked list is empty
+        cout<<"Deletion is not possible because the node passed is not found in the linked list."<<endl;
+}
+
+int main()
+{
+    CDLL mylist;
+    mylist.insertAtEnd(3);
+    mylist.insertAtEnd(6);
+    mylist.insertAtEnd(9);
+    mylist.insertAtEnd(12);
+    mylist.insertAtEnd(15);
+    mylist.deleteNode(mylist.searchNodeWithItem(55));
+    mylist.displayList();
+    return 0;
+}
