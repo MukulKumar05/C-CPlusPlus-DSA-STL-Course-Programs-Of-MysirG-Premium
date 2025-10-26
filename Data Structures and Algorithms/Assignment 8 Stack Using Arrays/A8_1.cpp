4c3623ca0607fce8d939566aa5d9b05754a46953
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-10-26 16:50:06 +0530

    Create A8_1.cpp

---
+++ Data Structures and Algorithms/Assignment 8 Stack Using Arrays/A8_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+/*
+A8.1 Define a class Stack with capacity, top and ptr pointer as member
+variables. Implement stack using array.
+
+(Completed)
+*/
+class Stack
+{
+    private:
+        int capacity, top;
+        int *ptr;
+};
+
+int main()
+{
+    
+    return 0;
+}
