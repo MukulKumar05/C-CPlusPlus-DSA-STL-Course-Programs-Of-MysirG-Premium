475c3b4dba827e58bd061525ce9ce2bddebfa572
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-10-31 15:01:02 +0530

    Add files via upload

---
+++ Data Structures and Algorithms/Assignment 8 Stack Using Arrays/A8_6.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+#define STACK_OVERFLOW 1
+#define STACK_UNDERFLOW -1
+
+/*
+A8.6 In question 1, define a destructor to deallocate the memory.
+
+Problem Summary: define destructor
+
+(Completed)
+*/
+class Stack
+{
+    private:
+        int capacity, top;
+        int *ptr;
+    public:
+        Stack(int);
+        ~Stack();
+        bool isFull();
+        void push(int);
+        void displayStackElements();
+        int peekTopElement();
+        bool isEmpty();
+        int pop();
+};
+Stack::Stack(int cap)
+{
+    if(cap<=0)
+        cap=10;
+    capacity=cap;
+    top=-1;
+    ptr=new int[cap];
+}
+Stack::~Stack()
+{
+    delete []ptr;
+}
+bool Stack::isFull()
+{
+    return capacity-1==top;
+}
+void Stack::push(int data)
+{
+    if(isFull())
+        throw STACK_OVERFLOW;
+    ptr[top+1]=data;
+    top++;
+}
+void Stack::displayStackElements()
+{
+    int i;
+    for(i = 0; i <= top; i++)
+        cout<<ptr[i]<<" ";
+}
+bool Stack::isEmpty()
+{
+    return top==-1;
+}
+int Stack::peekTopElement()
+{
+    if(isEmpty())
+        throw STACK_UNDERFLOW;
+    return ptr[top];
+}
+int Stack::pop()
+{
+    if(isEmpty())
+        throw STACK_UNDERFLOW;
+    return ptr[top--];
+}
+int main()
+{
+    Stack s1(5);
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    try
+    {
+        cout<<s1.pop()<<endl;
+    }
+    catch(int e)
+    {
+        cout<<"Cannot pop top element because the stack is empty."<<endl;
+    }
+    s1.displayStackElements();
+    return 0;
+}
