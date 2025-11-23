cef1fcc4b43f038711400c01dcd6d7a8790fd869
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-11-23 16:12:48 +0530

    Add files via upload

---
+++ Data Structures and Algorithms/Assignment 10 Queue Using Arrays/A10_9.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+#define ARRAY_OVERFLOW 1
+#define ARRAY_UNDERFLOW -1
+
+/*
+A10.9 In question 1, define a method to check queue underflow.
+
+Problem Summary: isQueueUnderflow()
+(Completed)
+*/
+class Queue
+{
+    private:
+        int capacity, front, rear;
+        int *ptr;
+    public:
+        Queue(int);
+        void insertRear(int);
+        bool isFull();
+        void displayElements();
+        ~Queue();
+        int viewRearElement();
+        bool isEmpty();
+        int viewFrontElement();
+        void deleteFrontElement();
+        bool isQueueOverflow();
+        bool isQueueUnderflow();
+};
+Queue::Queue(int cap){
+    if(cap<=0)
+        cap=10;
+    capacity=cap;
+    front=-1;
+    rear=-1;
+    ptr=new int[cap];
+}
+bool Queue::isFull()
+{
+    return capacity-1==rear && front == 0;
+}
+void Queue::insertRear(int data)
+{
+    if(isFull())
+        throw ARRAY_OVERFLOW;
+    if(isEmpty())
+        front++;
+    ptr[rear+1]=data;
+    rear++;
+}
+void Queue::displayElements()
+{
+    int i;
+    for(i = 0; i <= rear; i++)
+        cout<<ptr[i]<<" ";
+}
+Queue::~Queue()
+{
+    delete []ptr;
+}
+int Queue::viewRearElement()
+{
+    if(isEmpty())
+        throw ARRAY_UNDERFLOW;
+    return ptr[rear];
+}
+bool Queue::isEmpty()
+{
+    return rear==-1 && front == -1;
+}
+int Queue::viewFrontElement()
+{
+    if(isEmpty())
+        throw ARRAY_UNDERFLOW;
+    return ptr[front];
+}
+void Queue::deleteFrontElement()
+{
+    int i;
+    for(i=0;i<rear;i++)
+    {
+        ptr[i]=ptr[i+1];
+    }
+    rear--;
+}
+bool Queue::isQueueOverflow()
+{
+    return isFull();
+}
+bool Queue::isQueueUnderflow()
+{
+    return isEmpty();
+}
+
+int main()
+{
+    Queue q1(5);
+    q1.insertRear(10);
+    q1.insertRear(20);
+    q1.insertRear(30);
+    q1.insertRear(40);
+    q1.insertRear(50);
+    if(q1.isQueueUnderflow())
+        cout<<"Queue Underflow"<<endl;
+    else
+        cout<<"There are elements in queue."<<endl;
+    return 0;
+}
