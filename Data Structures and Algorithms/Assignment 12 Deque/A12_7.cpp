cb1540cf37c9c07858cc9256688dd2eb6f51ccf1
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2025-12-09 16:22:22 +0530

    Add files via upload

---
+++ Data Structures and Algorithms/Assignment 12 Deque/A12_7.cpp
@@ -0,0 +1,159 @@
+#include<iostream>
+using namespace std;
+#define QUEUE_UNDERFLOW -1
+#define EMPTY_QUEUE 0
+
+/*
+A12.7 In question 1, define a method to get front element.
+
+Problem Summary: getFrontElement()
+(Completed)
+*/
+
+struct node
+{
+    node *prev;
+    int item;
+    node *next;
+};
+
+class Deque
+{
+    private:
+        node *front;
+        node *rear;
+    public:
+        Deque();
+        void insertFront(int);
+        void deleteFront();
+        void display();
+        ~Deque();
+        void insertRear(int);
+        void deleteRear();
+        int getFrontElement();
+};
+Deque::Deque()
+{
+    front = nullptr;
+    rear = nullptr;
+}
+void Deque::insertFront(int data)
+{
+    node *newPtr = new node;
+    newPtr->prev = nullptr;
+    newPtr->item = data;
+    newPtr->next = front;
+    if(front) // queue has >= 1 elements
+    {
+        front->prev = newPtr;
+    }
+    else // queue is empty
+    {
+        rear = newPtr;
+    }
+    front = newPtr;
+}
+void Deque::display()
+{
+    if(front == nullptr)
+        cout<<"Queue is empty."<<endl;
+    else
+    {
+        node *ptr = front;
+        while(ptr)
+        {
+            cout<<ptr->item<<" ";
+            ptr = ptr->next;
+        }
+    }
+}
+void Deque::deleteFront()
+{
+    if(front) // if queue has >= 1 elements.
+    {
+        if(front->next)  // > 1 elements in queue
+        {
+            node *ptr = front;
+            front->next->prev = nullptr;
+            front = front->next;
+            delete ptr;
+        }
+        else // only 1 element is present in queue
+        {
+            rear = nullptr;
+            delete front;
+            front = nullptr;
+        }
+    }
+    else // queue is empty
+        throw QUEUE_UNDERFLOW;
+}
+Deque::~Deque()
+{
+    while(front)
+        deleteFront();
+}
+void Deque::insertRear(int data)
+{
+    node *newPtr = new node;
+    newPtr->item = data;
+    newPtr->next = nullptr;
+    if(front) // if there are >= 1 elements in the queue
+    {
+        newPtr->prev = rear;
+        rear->next = newPtr;
+        rear = newPtr;
+    }
+    else // if queue is empty
+    {
+        newPtr->prev = nullptr;
+        front = newPtr;
+        rear = newPtr;
+    }
+}
+void Deque::deleteRear()
+{
+    if(front) // if queue has >= 1 elements
+    {
+        if(front->next) // there are > 1 elements in the queue
+        {
+            node *ptr = rear;
+            ptr->prev->next = nullptr;
+            rear = rear->prev;
+            delete ptr;
+        }
+        else // there is only 1 element in the queue
+        {
+            rear = nullptr;
+            delete front;
+            front = nullptr;
+        }
+    }
+    else // if queue is empty
+        throw QUEUE_UNDERFLOW;
+}
+int Deque::getFrontElement()
+{
+    if(front)
+        return front->item;
+    throw EMPTY_QUEUE;
+}
+
+int main()
+{
+    Deque d1;
+    d1.insertRear(3);
+    d1.insertRear(6);
+    d1.insertRear(9);
+    d1.insertRear(12);
+    try
+    {
+        d1.getFrontElement();
+        cout<<"Front element of queue is "<<d1.getFrontElement()<<endl;
+    }
+    catch(int e)
+    {
+        cout<<"Queue is empty."<<endl;
+    }
+    return 0;
+}
