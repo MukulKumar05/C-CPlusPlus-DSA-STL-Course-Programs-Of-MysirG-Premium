b2c1b7742377173e11f5b23c8d58b643a09e0472
MukulKumar05 <88192678+MukulKumar05@users.noreply.github.com>
2026-01-25 17:01:17 +0530

    Add files via upload

---
+++ Data Structures and Algorithms/Assignment 17 Graph Matrix/A17_4.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+#define INVALID_VERTICES_COUNT 0
+#define INVALID_EDGES_COUNT 1
+#define INVALID_NODE_INDEX
+
+/*
+A17.4 In question 1, define a method to print all the adjacent nodes
+of a given node.
+
+Problem Summary: define printAllAdjacentNodesOfNode(int nodeIndex)
+(Completed)
+*/
+class Graph
+{
+    private:
+        int v_count, e_count, **adj;
+    public:
+        Graph();
+        void createGraph(int, int, int**);
+        ~Graph();
+        void printGraphMatrix();
+        void printAllAdjacentNodesOfNode(int);
+};
+Graph::Graph()
+{
+    v_count = 0;
+    e_count = 0;
+    adj = nullptr;
+}
+void Graph::createGraph(int verticesCount, int edgesCount, int **a)
+{
+    if(verticesCount <= 0)
+        throw INVALID_VERTICES_COUNT;
+    if(edgesCount < 0)
+        throw INVALID_EDGES_COUNT;
+    v_count = verticesCount;
+    e_count = edgesCount;
+    adj = new int*[v_count];
+    for(int i = 0; i < v_count; i++)
+    {
+        adj[i] = new int[v_count];
+    }
+    for(int i = 0; i < v_count; i++)
+    {
+        for(int j = 0; j < v_count; j++)
+        {
+            adj[i][j] = a[i][j];
+        }
+    }
+}
+Graph::~Graph()
+{
+    int i;
+    for(i = 0; i < v_count; i++)
+    {
+        delete []adj[i];
+    }
+    delete []adj;
+}
+void Graph::printGraphMatrix()
+{
+    for(int i = 0; i < v_count; i++)
+    {
+        for(int j = 0; j < v_count; j++)
+        {
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+void Graph::printAllAdjacentNodesOfNode(int nodeIndex)
+{
+    if(nodeIndex < 0 || nodeIndex >= v_count)
+        throw INVALID_NODE_INDEX;
+    cout<<"All adjacent nodes of node "<<nodeIndex<<" are: ";
+    for(int i = 0; i < v_count; i++)
+    {
+        if(adj[nodeIndex][i] == 1)
+        {
+            cout<<i<<", ";
+        }
+    }
+    cout<<"\b\b  ";
+    cout<<endl;
+}
+
+int main()
+{
+    Graph g1;
+    int arr[5][5] = {{0, 1, 1, 0, 0}, {1, 0, 0, 0, 1}, {1, 0, 0, 1, 1}, {0, 0, 1, 0, 1}, {0, 1, 1, 1, 0}};
+    int **a = new int*[5];
+    for(int i = 0; i < 5; i++)
+    {
+        a[i] = new int[5];
+    }
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            a[i][j] = arr[i][j];
+        }
+    }
+    g1.createGraph(5, 6, a);
+    for(int i = 0; i < 5; i++)
+    {
+        delete []a[i];
+    }
+    delete []a;
+    // g1.printGraphMatrix();
+    g1.printAllAdjacentNodesOfNode(1);
+    return 0;
+}
